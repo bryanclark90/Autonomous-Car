dfdd27bb655e7f2ca096dc9e05721c497091acfc
Kyle Finch <finch.kyle14@gmail.com>
2016-04-28 22:39:04 -0700

    Adding Andy's test project. It buildsgit status Also including FreeRTOS modified to Andy's specs

---
+++ FreeRTOSTest.X/build/default/production/main.o.d
@@ -0,0 +1,13 @@
+build/default/production/main.o:  \
+main.c  \
+Source/include/FreeRTOS.h  \
+ Source/include/projdefs.h  \
+Source/include/FreeRTOSConfig.h  \
+ Source/include/portable.h  \
+Source/portable/MPLAB/PIC32MX/portmacro.h  \
+ Source/include/mpu_wrappers.h  \
+FreeRTOSConfig.h  \
+Source/include/task.h  \
+ Source/include/list.h  \
+Source/include/queue.h  \
+ Source/include/ConfigPerformance.h 
